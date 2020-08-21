463ed85c552d85bef76f5b8597a6304c6a7fc7c6
ImagineTitanz <rriverab1800@alumno.ipn.mx>
2020-08-21 18:32:46 -0500

    linking platformio's proyect with github repository

---
+++ wifiRing/src/main.cpp
@@ -0,0 +1,13 @@
+/* ImagineTitanz
+Dispositivo con internet que le permite al usuario recibir señales a travez de una red wifi
+*/
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
